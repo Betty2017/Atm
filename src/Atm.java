e69b14d72d0d2a2471eda2b28d340e829a22184a
Betty Negatu <Betty Negatu>
2017-06-07 06:48:55 -0400

    Second Commit

--- src/Atm.java
+++ src/Atm.java
@@ -8,6 +8,7 @@ public class Atm {
 	Account a = new Account();
 	Account b = new Account();
 	Account c = new Account();
+	
 	public void store(){
 		a.setAccountNumber(123456);
 		a.setUserName("Betty");
@@ -33,37 +34,136 @@ public class Atm {
 		
 	System.out.println("Enter Account Number");
 	long accNumber = sc.nextLong();
-	int count =1;
-	
-	if(accNumber==a.getAccountNumber()||accNumber==b.getAccountNumber()||accNumber==c.getAccountNumber())
+
+	while (accNumber != a.getAccountNumber() && accNumber != b.getAccountNumber() && accNumber != c.getAccountNumber())
+		
 	{
+		System.out.println("try another accout Number.Y/N");
+		accNumber = sc.nextLong(); 
+		
+	}
+	
+	int pin = checkacount(accNumber);
+	
 	System.out.println("Enter Your PIN");
 	int pinNumber = sc.nextInt();
-	int pinStored=0;
-	if(accNumber==123456)
-	{ pinStored=a.getPin();
-	 	while(pinNumber == pinStored && count> 3)
-	 	{
-	 		System.out.println("Enter another PIN");
-	 		pinNumber = sc.nextInt();
-	 		count ++;
-	 	}
-	 	System.out.println("Sorry Account Blocked");
-	 	
+	boolean result = checkPin(pinNumber,pin);
+	if (result==true)
+	{
+		System.out.print("========================\n"
+				 +"ATM Menu: \n \n"
+				 + "1. Check Account Balance \n"
+				 + "2. Withdraw Money \n"
+				 + "3. Deposit Money \n"
+				 + "4. Exit \n"
+				 + "========================\n");
+		int option= sc.nextInt();
+		if (accNumber == a.getAccountNumber())
+		{
+				switch(option){  
+			    case 1: System.out.println("Your accout balance is " + a.getBalance());
+			    break;  
+			    case 2:System.out.println("Your accout balance is " + a.getBalance());
+			    	System.out.println("Enter withdraw amount");
+						double witAmount = sc.nextDouble();
+						double withdrawBalance;
+						withdrawBalance = a.getBalance() - witAmount;
+				    	System.out.println("Your Current Account balance is "+ withdrawBalance);
+				    	break;  
+			    case 3: System.out.println("Your accout balance is " + a.getBalance());
+			    	System.out.println("Enter deposit amount");
+						double depAmount = sc.nextDouble();
+						double depositBalance;
+						depositBalance = a.getBalance() + depAmount ;
+				    	System.out.println("Your Current  Account balance is "+ depositBalance);
+						break;  
+			    case 4: System.out.println("\nThank you for banking with us!\n");break; 
+			    default:System.out.println("Not valid entry");  
+			    }  
+			
+		}
+		else if (accNumber == b.getAccountNumber())
+		{
+			    switch(option){  
+			    case 1: System.out.println("Your accout balance is " + b.getBalance());break;  
+			    case 2:System.out.println("Your accout balance is " + b.getBalance());
+			    	System.out.println("Enter withdraw amount");
+						double witAmount = sc.nextDouble();
+						double withdrawBalance;
+						withdrawBalance = b.getBalance() - witAmount;
+				    	System.out.println("Your Account balance is "+ withdrawBalance);
+				    	break;  
+			    case 3:System.out.println("Your accout balance is " + b.getBalance()); 
+			    	System.out.println("Enter deposit amount");
+						double depAmount = sc.nextDouble();
+						double depositBalance;
+						depositBalance = b.getBalance() + depAmount ;
+				    	System.out.println("Your Account balance is "+ depositBalance);
+						break; 
+			    case 4: System.out.println("\nThank you for banking with us!\n");break; 
+			    default:System.out.println("Not valid entry");  
+			    }  
+			
+		}
+
+		else 
+		{
+			    switch(option){  
+			    case 1: System.out.println("Your accout balance is " + c.getBalance());break;  
+			    case 2:System.out.println("Your accout balance is " + c.getBalance());
+			    		System.out.println("Enter withdraw amount");
+						double witAmount = sc.nextDouble();
+						double withdrawBalance;
+						withdrawBalance = c.getBalance() - witAmount;
+				    	System.out.println("Your Account balance is "+ withdrawBalance);
+				    	break;  
+			    case 3: System.out.println("Your accout balance is " + c.getBalance());
+			    		System.out.println("Enter deposit amount");
+						double depAmount = sc.nextDouble();
+						double depositBalance;
+						depositBalance = c.getBalance() + depAmount ;
+				    	System.out.println("Your Account balance is "+ depositBalance);
+						break; 
+			    case 4: System.out.println("\nThank you for banking with us!\n");break; 
+			    default:System.out.println("Not valid entry");  
+			    } 
+		}
+		 
 	}
-	else if (accNumber==654321)
-		 pinStored=b.getPin();
-	else
-		pinStored=c.getPin();
-	}
-	System.out.println("1: Check Balance");
-	int check= sc.nextInt();
-	System.out.println("2: Withdraw");
-	int withdeaw= sc.nextInt();
-	System.out.println("3: Deposit");
-	int deposit= sc.nextInt();
+	else 
+		
+	System.out.println("Maximum attempt reached. \n Your Account is blcoked ");
+	
+	
 	}
 	
 	
+	public boolean checkPin(int thePinNumber, int pin){
+		
+		if (thePinNumber==pin)
+			return true;
+		else
+		{
+			int i=0;
+			while(i<2){
+				if (thePinNumber==pin)
+					return true;	
+				else{
+					System.out.println("try again");
+					 thePinNumber = sc.nextInt();
+				}
+				i++;
+			}
+		}
+		return false;
+	}
+	public int checkacount(long theAccountNumer){
+		if (theAccountNumer == a.getAccountNumber())
+			return a.getPin();
+		else if (theAccountNumer == b.getAccountNumber())
+			return b.getPin();
+		else 
+			return c.getPin();
+	}
 	
 	}
--- src/Receipt.java
+++ src/Receipt.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
 
 public class Receipt {
-
-}
+	Scanner sc = new Scanner(System.in);
+	
+	Receipt(){
+		System.out.println("Print Receipt Y/N");
+		String choice = sc.nextLine();
+		
+		if(choice.equalsIgnoreCase("y"))
+		{
+		System.out.println("-------------------------------------------");
+		System.out.println("|         Java Bank ATM Receipt           |");
+		System.out.println("|         Wednesday, December 2, 2015     |");
+		System.out.println("|                                         |");
+		System.out.println("|                                         |");
+		System.out.println("|         Account Number:      1234567    |");
+		System.out.println("|         Customer:     John Q. Public    |");
+		System.out.println("|         Transaction Type:    Deposit    |");
+		System.out.println("|         Transaction Amount:  $500.00    |");
+		System.out.println("|         Account Balance:   $1,500.00    |");
+		System.out.println("|                                         |");
+		System.out.println("|                                         |");
+		System.out.println("|         Thank you for banking with us   |");
+		System.out.println("|              Have a coffee day          |");
+		System.out.println("|                                         |");
+		System.out.println("------------------------------------------");
+		}
+		else System.out.println("\nThank you for banking with us!\n");
+}}
